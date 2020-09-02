33dbdbf00417593c2588e4233d60c677a0c03abc
Anna Agozzino <anna.agozzino91@gmail.com>
2020-09-02 17:40:13 +0200

    Funzione slider Image + frecce

--- main.js
+++ main.js
@@ -1,53 +1,77 @@
 $(document).ready(function(){
-  // click icona prev o press tasto left
+  function prevImage() {
+    // salvo img con class="active"
+      // rimuovo class="active" dall'immagine e dal circle
+    var imgActive = $("img.active");
+    imgActive.removeClass("active");
+    var circleActive = $(".nav > i.active");
+    circleActive.removeClass("active");
+    // se img con class="active" ha class="first" -> img precedente = img con class="last"
+    if (imgActive.hasClass("first") && circleActive.hasClass("first")) {
+      var prevImg = $("img.last");
+      var prevCircle = $(".nav > i.last");
+    } else {
+      // salvo img precedente a quella con class="active"
+      var prevImg = imgActive.prev();
+      var prevCircle = circleActive.prev();
+    };
+    // a img precedente aggiungo la class="active"
+    prevImg.addClass("active");
+    prevCircle.addClass("active");
+  };
+
+  function nextImage(){
+    // salvo img con class="active"
+      // rimuovo class="active"
+    var imgActive = $("img.active");
+    imgActive.removeClass("active");
+    var circleActive = $(".nav > i.active");
+    circleActive.removeClass("active");
+    // se img con class="active" ha class="last" -> img successiva = img con class="first"
+    if (imgActive.hasClass("last") && circleActive.hasClass("last")) {
+      var nextImg = $("img.first");
+      var nextCircle = $(".nav > i.first");
+    } else {
+      // salvo img precedente a quella con class="active"
+      var nextImg = imgActive.next();
+      var nextCircle = circleActive.next();
+    };
+    // a img precedente aggiungo la class="active"
+    nextImg.addClass("active");
+    nextCircle.addClass("active");
+  };
+
+  // click icona prev
   $(".prev").click(
     function(){
-      // salvo img con class="active"
-        // rimuovo class="active" dall'immagine e dal circle
-      var imgActive = $("img.active");
-      imgActive.removeClass("active");
-
-      // se img con class="active" ha class="first" -> img precedente = img con class="last"
-      if (imgActive.hasClass("first")) {
-        var prevImg = $("img.last");
-      } else {
-        // salvo img precedente a quella con class="active"
-        var prevImg = imgActive.prev();
-      };
-      // a img precedente aggiungo la class="active"
-      prevImg.addClass("active");
-
-      var circleActive = $(".nav > i.active");
-      circleActive.removeClass("active");
-      if (circleActive.hasClass("first")) {
-        var prevCircle = $(".nav > i.last");
-      } else{
-        var prevCircle = circleActive.prev();
-      };
-      prevCircle.addClass("active");
+      prevImage();
     }
   );
 
-  // click icona next o press tasto right
+  // click icona next
   $(".next").click(
     function(){
-      // salvo img con class="active"
-        // rimuovo class="active"
-      var imgActive = $("img.active");
-      imgActive.removeClass("active");
-
-      // se img con class="active" ha class="last" -> img successiva = img con class="first"
-      if (imgActive.hasClass("last")) {
-        var nextImg = $("img.first");
-      } else {
-        // salvo img precedente a quella con class="active"
-        var nextImg = imgActive.next();
-      };
-      // a img precedente aggiungo la class="active"
-      nextImg.addClass("active");
+      nextImage();
     }
   );
 
+  // press freccia sinistra
+  $(document).keydown(
+    function(key){
+      switch (key.which) {
+        case 37:
+          prevImage();
+          break;
+        case 39:
+          nextImage();
+          break;
+        default:
+        return;
+      }
+      key.preventDefault();
+    }
+  );
+  // press freccia destra
 });
 
 // function slidePrev(selettore) {
